638204b0d8433894814ee980093915c461347db5
shivamsingha <3351235-shivamsingha@users.noreply.gitlab.com>
2019-01-20 22:59:29 +0530

    First actual commit. Grommet broilerplate.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Grommet } from 'grommet';
+
+class App extends Component {
+  render() {
+    return (
+      <Grommet plain>
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        </Grommet>
+    );
+  }
+}
+
+export default App;
