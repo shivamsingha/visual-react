23286aadf091896ed019aae241cad96e0b21ad72
shivamsingha <3351235-shivamsingha@users.noreply.gitlab.com>
2019-01-21 21:51:26 +0530

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import { Grommet } from 'grommet';
+import React, { Component } from 'react';
+import { Box, Button, Diagram, Heading, Grommet, Stack } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    align='center' 
+    background='neutral-1' 
+    direction='row-responsive' 
+    elevation='medium' 
+    justify='between' 
+    tag='header'
+    pad='medium'
+    {...props}
+  />
+);
+
+const SideBar = (props) => (
+  <Box
+    align='center'
+    background='accent-1'
+    width='medium'
+    elevation='small'
+    justify='center'
+    {...props}
+  />
+);
 
 class App extends Component {
+  /*returnStack() {
+    return <linkedlist/>;
+  }*/
+
   render() {
     return (
-      <Grommet plain>
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        </Grommet>
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            Texteru
+            <Heading level='3' margin='none'>Heading</Heading>
+            <Button icon={<Notification />} onclick='() => {}' />
+          </AppBar>
+          <Box direction='row' flex overflow={{horizontal:'hidden'}}>
+            <Box flex align='center' justify='center'>
+              body
+            </Box>
+            <SideBar>
+              {/*this.returnStack()*/}
+            </SideBar>
+          </Box>
+        </Box>
+      </Grommet>
     );
   }
 }
 
+class linkedlist extends Component {
+  render() {
+    return (
+      <Stack fill>
+        <Box>
+          <Box direction='row'>
+          </Box>
+        </Box>
+      </Stack>
+    );
+  }
+}
 export default App;
--- src/__tests__/App.js
+++
@@ -1,5 +0,0 @@
-import App from '../App';
-
-it('renders without crashing', () => {
-    ReactDOM.render(<App />, document.getElementById('root'));
-});
