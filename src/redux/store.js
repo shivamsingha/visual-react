4e1226809ee5e2985fa5da7a3dc12b779728e188
shivamsingha <shivamgit@protonmail.com>
2019-06-10 12:18:25 +0530

    Update store to enable redux devtools
    Signed-off-by: shivamsingha <shivamgit@protonmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default (process.env.NODE_ENV === 'development')
+  ? createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+  : createStore(
+    rootReducer
+  )
